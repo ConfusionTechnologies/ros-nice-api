a84fff574445aa900d15ef770bc154629c9fd77e
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-06-28 12:56:35 +0000

    red ball anim can be seen via rqt image view

--- aiortc_ros/aiortc_ros/recv.py
+++ aiortc_ros/aiortc_ros/recv.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 import sys
+from time import time
 
+import cv2
+import numpy as np
+from cv_bridge import CvBridge
 import rclpy
-from rclpy.qos import QoSPresetProfiles
 from rclpy.node import Node
 
 from aiortc import MediaStreamTrack
@@ -19,6 +22,8 @@ from nicepynode import Job, JobCfg
 # depending on audio or video track
 
 NODE_NAME = "TEST"
+
+cv_bridge = CvBridge()
 
 
 class VideoRecvTrack(MediaStreamTrack):
@@ -49,9 +54,13 @@ class RTCReceiver(Job[RTCRecvConfig]):
     def attach_behaviour(self, node, cfg: RTCRecvConfig):
         super(RTCReceiver, self).attach_behaviour(node, cfg)
 
-        self._publisher = node.create_publisher(
-            Image, cfg.topic, QoSPresetProfiles.SENSOR_DATA.value
-        )
+        self._spd = (250, 10)  # (x,y) px per second
+        self._pos = (20, 0)  # in px
+        self._size = 10
+
+        self._image = np.zeros((480, 640, 3), np.uint8)
+
+        self._publisher = node.create_publisher(Image, cfg.topic, 30)
         try:
             self._timer = node.create_timer(1.0 / cfg.max_rate, self._timer_cb)
         except ZeroDivisionError:
@@ -65,7 +74,28 @@ class RTCReceiver(Job[RTCRecvConfig]):
         node.destroy_timer(self._timer)
 
     def _timer_cb(self):
-        msg = Image()
+        now = time()
+        try:
+            delta = now - self._prev
+        except AttributeError:
+            delta = 0.0
+        self._prev = now
+        self._pos = (
+            (self._pos[0] + self._spd[0] * delta) % self._image.shape[1],
+            (self._pos[1] + self._spd[1] * delta) % self._image.shape[0],
+        )
+
+        # reset image canvas
+        self._image[...] = (0, 255, 0)
+        cv2.circle(
+            self._image,
+            (int(self._pos[0]), int(self._pos[1])),
+            self._size,
+            (0, 0, 255),
+            -1,
+        )
+
+        msg = cv_bridge.cv2_to_imgmsg(self._image, encoding="bgr8")
         self._publisher.publish(msg)
 
     def on_params_change(self, node, changes):
@@ -84,7 +114,7 @@ def main(args=None):
 
     node = Node(NODE_NAME)
 
-    cfg = RTCRecvConfig()
+    cfg = RTCRecvConfig(max_rate=120)
     RTCReceiver(node, cfg)
 
     rclpy.spin(node)
